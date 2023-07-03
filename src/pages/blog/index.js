b2dcdd244a228240a49e0e7fda58a9504ef8185a
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-07-03 23:42:29 +0530

    Fetch posts from file system and frontmatter

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,9 +1,9 @@
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
 
 export default function BlogPage() {
   return (
     <Layout>
       <h1>Blog</h1>
     </Layout>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import Layout from '../components/Layout'
+import matter from 'gray-matter';
+import fs from 'fs'
+import path from 'path'
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({posts}) {
+  console.log("posts",posts)
   return (
     <Layout>
       <h1>Hello World</h1>
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    
+    // console.log("markdownWithMeta",markdownWithMeta)
+    
+    const { data: frontmatter } = matter(markdownWithMeta);
+    
+    return {
+        slug,
+        frontmatter,
+    };
+});
+
+return {
+    props: {
+      posts,
+    },
+  };
 }
