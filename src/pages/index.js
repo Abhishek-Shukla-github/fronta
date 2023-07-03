58bb9ff197e472b2e6d97197d7d2816e216ceb31
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-07-04 00:33:01 +0530

    Render Posts on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-import Layout from '../components/Layout'
-import matter from 'gray-matter';
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import Post from "@/components/Post";
+import matter from "gray-matter";
+import path from "path";
+import { sortByDate } from '../../utils'
 
-// const inter = Inter({ subsets: ['latin'] })
-
-export default function Home({posts}) {
-  console.log("posts",posts)
+export default function Home({ posts }) {
+  console.log("posts", posts);
   return (
     <Layout>
-      <h1>Hello World</h1>
-    </Layout>
-  )
-}
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
 
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link
+        href="/blog"
+        className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full"
+      >
+        All Posts
+      </Link>
+    </Layout>
+  );
+}
 
 export async function getStaticProps() {
   const files = fs.readdirSync(path.join("posts"));
@@ -28,20 +40,19 @@ export async function getStaticProps() {
       "utf-8"
     );
 
-    
     // console.log("markdownWithMeta",markdownWithMeta)
-    
-    const { data: frontmatter } = matter(markdownWithMeta);
-    
-    return {
-        slug,
-        frontmatter,
-    };
-});
 
-return {
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   };
 }
